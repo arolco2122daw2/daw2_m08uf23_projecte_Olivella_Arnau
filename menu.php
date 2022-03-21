b0265d341e6fed05867b1110c9e403b65054e54d
arolco <arolco@zend-arolco.fjeclot.net>
2022-03-21 19:52:19 +0100

    solucio errors

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
 	<body>
 		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
 		<h3> <a href="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/visualitza.php">Veure Usuari</a> </h3>
-		<h3> <a href=""http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/afegir.php">Afegir Usuari</a> </h3>
+		<h3> <a href="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/afegir.php">Afegir Usuari</a> </h3>
 		<h3> <a href="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/modificar.php">Modificar Usuari</a> </h3>
 		<h3> <a href="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/eliminar.php">Eliminar Usuari</a> </h3>
 		<a href="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/index.php">Torna a la pàgina inicial</a>

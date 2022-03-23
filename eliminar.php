f23fb3864eca24ad7f681c3a348c49600ac80feb
arolco <arolco@zend-arolco.fjeclot.net>
2022-03-23 12:33:13 +0100

    Ultimes modificacions perque funcioni correctament eliminar i modificar usuaris

--- eliminar.php
+++ eliminar.php
@@ -1,48 +1,45 @@
 <?php
-	require 'vendor/autoload.php';
-	use Laminas\Ldap\Ldap;
-	
-	ini_set('display_errors', 0);
-	#
-	# Entrada a esborrar: usuari 3 creat amb el projecte zendldap2
-	#
-	$uid = 'usr3';
-	$unorg = 'usuaris';
-	$dn = 'uid='.$_POST["uid"].',ou='.$_POST["uo"].',dc=fjeclot,dc=net';
-	#
-	#Opcions de la connexió al servidor i base de dades LDAP
-	$opcions = [
-		'host' => 'zend-arolco.fjeclot.net',
-		'username' => 'cn=admin,dc=fjeclot,dc=net',
-		'password' => 'fjeclot',
-		'bindRequiresDn' => true,
-		'accountDomainName' => 'fjeclot.net',
-		'baseDn' => 'dc=fjeclot,dc=net',		
-	];
-	#
-	# Esborrant l'entrada
-	#
-	$ldap = new Ldap($opcions);
-	$ldap->bind();
-	try{
-	    $ldap->delete($dn);
-	    echo "<b>Entrada esborrada</b><br>"; 
-	}catch(Exception $error){
-	   echo "<b>Aquesta entrada no existeix</b><br>";
-	}
-	echo "<a href=\"http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/menu.php\">Torna al menú</a>";
+    require 'vendor/autoload.php';
+    use Laminas\Ldap\Attribute;
+    use Laminas\Ldap\Ldap;
+    
+    ini_set('display_errors', 0);
+    #
+    # Entrada a esborrar: usuari 3 creat amb el projecte zendldap2
+    #
+    if ($_GET['usr'] && $_GET['ou']){
+        $uid = $_GET['usr'];
+        $unorg = $_GET['ou'];
+        $dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
+        #
+        #Opcions de la connexió al servidor i base de dades LDAP
+        $opcions = [
+            'host' => 'zend-arolco.fjeclot.net',
+            'username' => 'cn=admin,dc=fjeclot,dc=net',
+            'password' => 'fjeclot',
+            'bindRequiresDn' => true,
+            'accountDomainName' => 'fjeclot.net',
+            'baseDn' => 'dc=fjeclot,dc=net',
+        ];
+        #
+        # Esborrant l'entrada
+        #
+        $ldap = new Ldap($opcions);
+        $ldap->bind();
+        if ($ldap->delete($dn))	echo "<b>Entrada esborrada</b><br>";
+    }else echo "<b>Aquest usuari no existeix</b><br><br>";
+    
 ?>
 
 <html>
 	<head>
-		<title> Elimina l'usuari que vulguis de la base de dades LDAP</title>
+		<title> ESBORRANT USUARIS DE LA BASE DE DADES LDAP</title>
 	</head>
 	<body>
 		<form action="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/eliminar.php" method="GET">
-			UID: <input required type="text" name="uid"><br>
-			Unitat Organitzativa: <input required type="text" name="uo"><br>
-			<input type="submit" value="Envia" />
-			<input type="reset" value="Neteja" />
+			Unitat organitzativa: <input type="text" name="ou"><br>
+			Usuari: <input type="text" name="usr"><br>
+			<input type="submit"/>
 		</form>
 		
 				<br><br>Tornar al inici <button> <a href="menu.php">INICI</a></button> <br><br>
--- modificar.php
+++ modificar.php
@@ -1,4 +1,3 @@
-
 <?php
 	require 'vendor/autoload.php';
 	use Laminas\Ldap\Attribute;
@@ -6,66 +5,70 @@
 	
 	ini_set('display_errors', 0);
 	#
-	# Entrada a modificar
+	# Atribut a modificar --> Número d'idenficador d'usuari
 	#
-	$uid = $_POST["uid"];
-	$unorg = $_POST["uo"];
-	$dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
-	#
-	#Opcions de la connexió al servidor i base de dades LDAP
-	$opcions = [
-		'host' => 'zend-arolco.fjeclot.net',
-		'username' => 'cn=admin,dc=fjeclot,dc=net',
-		'password' => 'fjeclot',
-		'bindRequiresDn' => true,
-		'accountDomainName' => 'fjeclot.net',
-		'baseDn' => 'dc=fjeclot,dc=net',		
-	];
-	#
-	# Modificant l'entrada
-	#
-	$ldap = new Ldap($opcions);
-	$ldap->bind();
-	$entrada = $ldap->getEntry($dn);
-	if ($entrada){
-	    if($_POST["atribut"]== 'uidNumber' || $_POST["atribut"]== 'gidNumber' ){
-	      Attribute::setAttribute($entrada,$_POST["atribut"],(int)$_POST["atr"]);
-		  $ldap->update($dn, $entrada);
-	    }else{
-	        Attribute::setAttribute($entrada,$_POST["atribut"],$_POST["atr"]);
-	        $ldap->update($dn, $entrada);
-	    }
-		echo "Atribut modificat"; 
-	} else echo "<b>Aquesta entrada no existeix</b><br><br>";	
-	echo "<br><br><a href=\"http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/menu.php\">Torna al menú</a>";
+	if($_GET['ou'] && $_GET['usr']){	    
+	    
+	    $atribut=$_GET['MyRadio']; # El número identificador d'usuar té el nom d'atribut uidNumber
+    	$nou_contingut=$_GET['valor'];
+    	#
+    	# Entrada a modificar
+    	#
+    	$uid = $_GET['usr'];
+    	$unorg = $_GET['ou'];
+    	$dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
+    	#
+    	#Opcions de la connexió al servidor i base de dades LDAP
+    	$opcions = [
+    		'host' => 'zend-arolco.fjeclot.net',
+    		'username' => 'cn=admin,dc=fjeclot,dc=net',
+    		'password' => 'fjeclot',
+    		'bindRequiresDn' => true,
+    		'accountDomainName' => 'fjeclot.net',
+    		'baseDn' => 'dc=fjeclot,dc=net',		
+    	];
+    	#
+    	# Modificant l'entrada
+    	#
+    	$ldap = new Ldap($opcions);
+    	$ldap->bind();
+    	$entrada = $ldap->getEntry($dn);
+    	if ($entrada){
+    		Attribute::setAttribute($entrada,$atribut,$nou_contingut);
+    		$ldap->update($dn, $entrada);
+    		echo "Atribut modificat"; 
+    	}
+	}else echo "<b>Aquest usuari no existeix</b><br><br>";
+	
 ?>
 
 <html>
 	<head>
-		<title>Modifica els camps d'un usuari de la base de dades LDAP</title>
+		<title>MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP</title>
 	</head>
 	<body>
 		<form action="http://zend-arolco.fjeclot.net/daw2_m08_uf23_projecte_Olivella_Arnau/modificar.php" method="GET">
-			UID: <input required type="text" name="uid"><br>
-			Unitat Organitzativa: <input required type="text" name="uo"><br>
-			<fieldset>
-        		<legend>Escull un atribut:</legend>
-        		<label><input checked type="radio" name="atribut" value="uidNumber">UID Num</label>
-        		<label><input type="radio" name="atribut" value="gidNumber">Grup Num</label>
-        		<label><input type="radio" name="atribut" value="homeDirectory">Directori personal</label>
-        		<label><input type="radio" name="atribut" value="loginShell">SH</label>
-        		<label><input type="radio" name="atribut" value="cn">CN</label>
-        		<label><input type="radio" name="atribut" value="sn">SN</label>
-        		<label><input type="radio" name="atribut" value="givenName">Nom</label>
-        		<label><input type="radio" name="atribut" value="postalAdress">Adressa</label>
-        		<label><input type="radio" name="atribut" value="mobile">mobile</label>
-        		<label><input type="radio" name="atribut" value="telephoneNumber">telefon</label>
-        		<label><input type="radio" name="atribut" value="title">titol</label>
-        		<label><input type="radio" name="atribut" value="description">descripcio</label>
-			</fieldset>
-			Introdueix l'atribut excollit: <input required type="text" name="atr"><br>
-			<input type="submit" value="Envia" />
-			<input type="reset" value="Neteja" />
+			Unitat organitzativa: <input type="text" name="ou"><br>
+			Usuari: <input type="text" name="usr"><br><br><br>
+			
+			Atribut a modificar
+			<input type="radio" name="MyRadio" value="uidNumber">uidNumber<br>
+			<input type="radio" name="MyRadio" value="gidNumber">gidNumber<br>
+			<input type="radio" name="MyRadio" value="homeDirectory">Directori personal<br>
+			<input type="radio" name="MyRadio" value="loginShell">Shell<br>
+			<input type="radio" name="MyRadio" value="cn">cn<br>
+			<input type="radio" name="MyRadio" value="sn">sn<br>
+			<input type="radio" name="MyRadio" value="givenName">givenName<br>
+			<input type="radio" name="MyRadio" value="postalAddress">PostalAdress<br>
+			<input type="radio" name="MyRadio" value="mobile">Movil<br>
+			<input type="radio" name="MyRadio" value="telephoneNumber">Telefon<br>
+			<input type="radio" name="MyRadio" value="title">Title<br>
+			<input type="radio" name="MyRadio" value="description">Descripcio<br><br>
+			
+			Valor: <input type="text" name="valor"><br>
+			
+			
+			<input type="submit"/>
 		</form>
 		
 				<br><br>Tornar al inici <button><a href="menu.php">INICI</a></button> <br><br>
